24d4d9143e0ff9733ae7b4252aebd0e489192b7b
fatzzx <felipespinolafarias1@gmail.com>
2023-08-27 09:49:40 -0300

    a

--- Pilha/pilha.h
+++ Pilha/pilha.h
@@ -7,67 +7,123 @@
 typedef int tp_item;
 
 typedef struct{
-  int topo;
-  tp_item item[TAMANHO_MAX];
-}tp_pilha;
+    int topo;
+    tp_item item[MAX_PILHA];
+} tp_pilha;
 
-void inicializa_pilha(tp_pilha *pilha){
-  pilha->topo = -1;
+// inicia uma pilha, atribuindo -1 ao topo
+void inicializa_pilha(tp_pilha *p){
+    p->topo = -1;
 }
 
-int pilha_vazia(tp_pilha pilha){
-  if(pilha.topo == -1) return 1;
-  else return 0;
+// vê se a pilha está vazia, retornando 1 se estiver vazia ou 0, caso contrário
+int pilha_vazia(tp_pilha *p){
+    if (p->topo == -1)
+        return 1;
+    return 0;
 }
 
-int pilha_cheia(tp_pilha pilha){
-  if(pilha.topo == TAMANHO_MAX - 1) return 1;
-  else return 0;
+// vê se a pilha tá cheia, retornando 1 se estiver cheia ou 0, caso contrário
+int pilha_cheia(tp_pilha *p){
+    if (p->topo == MAX_PILHA - 1)
+        return 1;
+    return 0;
 }
 
-int pilha_vazia_ou_cheia(tp_pilha pilha){
-  if(pilha_cheia(pilha) || pilha_vazia(pilha)) return 1;
-  return 0;
+// Empilha um elemento
+int push(tp_pilha *p, tp_item e){
+    if (pilha_cheia(p))
+        return 0;
+    p->topo++;
+    p->item[p->topo] = e;
+    return 1;
 }
 
-int insere_pilha(tp_pilha *pilha, tp_item e){
-  if(pilha_cheia(*pilha)) return 0;
-  pilha->topo++;
-  pilha->item[pilha->topo] = e;
-  return 1;
+// Desempilha um elemento
+int pop(tp_pilha *p, tp_item *e){
+    if (pilha_vazia(p))
+        return 0;
+    *e = p->item[p->topo];
+    p->topo--;
+    return 1;
 }
 
-tp_item item_topo(tp_pilha pilha){
-
-  return pilha.item[pilha.topo];  
-
+// retorna o elemento que está no topo, sem desempilhar
+int top(tp_pilha *p, tp_item *e){
+    if (pilha_vazia(p))
+        return 0;
+    *e = p->item[p->topo];
+    return 1;
 }
 
-int remove_pilha(tp_pilha *pilha, tp_item *e){
-  if(pilha_vazia(*pilha))return 0;
-  *e = item_topo(*pilha);
-  pilha->topo --;
-  return 1;
+int altura_pilha(tp_pilha *p){
+    return p->topo + 1;
 }
 
-int remove_pilha2(tp_pilha *pilha){
-  if(pilha_vazia(*pilha))return 0;
-  pilha->topo --;
-  return 1;
+void imprime_pilha(tp_pilha p){
+    tp_item e;
+    while (pilha_vazia(&p) == 0){
+        pop(&p, &e);
+        cout << "\n" << e;
+    }
 }
 
-void imprimir_elementos(tp_pilha pilha){
-  while(!pilha_vazia(pilha)){
-    printf("%d ", item_topo(pilha));
-    remove_pilha2(&pilha);
-  }
+void retira_impares(tp_pilha *p){
+    tp_pilha paux;
+    inicializa_pilha(&paux);
+    tp_item e;
+    //desempilha a pilha original e armazena apenas os pares na pilha auxiliar
+    // ! é a mesma coisa de ==0 nesse caso, ou igual a not
+    while(!pilha_vazia(p)){
+    // p já é ponteiro, por isso não coloca o &
+    pop(p, &e);
+    if (e%2==0) //Se par
+    push(&paux, e);
+    } //fim do while
+
+    while (!pilha_vazia(&paux)){
+    pop(&paux, &e);
+    push(p,e);
+    }
 }
 
-int tamanho_pilha(tp_pilha pilha){
+int pilhas_iguais(tp_pilha p1, tp_pilha p2){
+    tp_item e1, e2;
 
-  return ++pilha.topo;
+    if (altura_pilha(&p1) != altura_pilha(&p2)) 
+    return 0;
+    
+    while(!pilha_vazia(&p1)){
+    pop(&p1, &e1);
+    pop(&p2, &e2);
+    if(e1 != e2) return 0;
+    }
 
+    return 1;
 }
+
+int empilha_pilhas(tp_pilha *p1, tp_pilha *p2){
+    tp_item e;
+    tp_pilha paux;
+    inicializa_pilha(&paux);
+    if(altura_pilha(p1)+altura_pilha(p2) > MAX_PILHA) return 0;
+    while (!pilha_vazia(p2)){
+    pop(p2, &e);
+    push(&paux, e);
+    }
+    while(!pilha_vazia(&paux)){
+    pop(&paux, &e);
+    push(p1, e);
+    }
+    return 1;
+}
+
+void zera_pilha(tp_pilha *p, tp_item *e){
+    while(!pilha_vazia(p)){
+        pop(p, e);
+    }
+}
+
 
 
 #endif
